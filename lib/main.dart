6ac949b91c77eed1a3161db4c98592a23a9a9bc6
Jerry Agbesi <jerryeagbesi@gmail.com>
2021-12-22 18:41:25 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main()  => runApp(NetflixClone());
+
+
+class NetflixClone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: ,
+    );
+  }
+}
